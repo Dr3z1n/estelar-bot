9268fd2ed2be7a3e9a13deadeed05fca88924d3d
Dr3z1n <102878964+Dr3z1n@users.noreply.github.com>
2022-10-06 14:02:35 -0300

    Add files via upload

---
+++ src/menu/pack.js
@@ -0,0 +1,202 @@
+const packs = (date, user, wame) => {
+    return `╭━━━━━━━━━━━━━━━━━━━╮
+┃🔥☄️𝙀𝙨𝙩⃟𝙚𝙡𝙖𝙧 𝙊𝙧𝙞⃟𝙤𝙣☄️🔥
+┃  
+┃ 🕐 𝐃𝐚𝐭𝐚: ${date}
+┃ 🙂 𝐔𝐬𝐮𝐚́𝐫𝐢𝐨: ${user}
+┃ 🌎 𝐖𝐚𝐦𝐞: ${wame}                                       
+┃
+┣━━ 🤩 𝑹𝑬𝑫𝑬𝑺 𝑺𝑶𝑪𝑰𝑨𝑰𝑺 🤩 ━╮
+┃
+┃ 📷 𝐈𝐧𝐬𝐭𝐚𝐠𝐫𝐚𝐦: @dr3z1n
+┃
+┃ 🤖𝐆𝐢𝐭𝐡𝐮𝐛:
+┃ 🌐https://linkebr.com/github
+┃
+┃ 🪀𝐆𝐫𝐮𝐩𝐨:
+┃ 🌐https://linkebr.com/grupoofv
+┃
+┃
+┣━━━━━━━━━━━━━━━━━━━╮
+┃ 𝒂𝒈𝒓𝒂𝒅𝒆𝒄𝒊𝒎𝒆𝒏𝒕𝒐 𝒑𝒂𝒓𝒂 
+┃ 𝒎𝒂𝒔𝒕𝒆𝒓𝒉𝒈 𝒑𝒐𝒓 𝒇𝒐𝒓𝒏𝒆𝒄𝒆𝒓
+┃ 𝒐𝒔 𝒑𝒂𝒄𝒌𝒔 
+┃ 𝒍𝒊𝒏𝒌 𝒕𝒆𝒍𝒆𝒈𝒓𝒂𝒎:
+┃ https://linkebr.com/mastrhg
+┃ 
+┃ 𝐓𝐮𝐭𝐨𝐫𝐢𝐚𝐥 𝐝𝐞 𝐜𝐨𝐦𝐨 𝐛𝐚𝐢𝐱𝐚𝐫
+┃ https://youtube.com/shorts/WOKRMLLaj2A?feature=share
+┃ 
+┃ vlw master ❤️
+┃ 
+┣━━━😈🔞 𝒑𝒂𝒄𝒌𝒔 𝒅𝒆 +18 🔞😈━━━╮
+┃ 
+┃ 🔞🔥 Pack da Larissa Sumpani
+┃ 📂 • Link pra baixar:
+┃ https://linkebr.com/zpALotp 
+┃ 
+┃ 🔞🔥 Pack da Lorena Apolinario
+┃ 📂 • Link pra baixar:
+┃ https://linkebr.com/NW51u5s8 
+┃ 
+┃ 🔞🔥 +1 Lomotif 🙊🔥🔞
+┃ 📂 • vídeo completo:
+┃ https://linkebr.com/TtM1Atq
+┃ 
+┃ 🔞🔥 Velha gozando baldes 😳🔥
+┃ 📂 • vídeo completo:
+┃ https://linkebr.com/prtAi6
+┃ 
+┃ 🔞🔥 Egrilo gostosa 😋🔥🔞🔞
+┃ 📂 • vídeo completo:
+┃ https://linkebr.com/zbtbo1
+┃ 
+┃ 🔞🔥 Xerecao kkk 🤭🔥🔞
+┃ 📂 • vídeo completo:
+┃ https://linkebr.com/em5nC
+┃ 
+┃ 🔞🔥 Pack da Elizabeth Borges 
+┃ 📂 • Link pra baixar:
+┃ https://linkebr.com/JI599we 
+┃ 
+┃ 🔞🔥 Pack da Julia Bhering
+┃ 📂 • Link pra baixar:
+┃ https://linkebr.com/0MIYQa4Y 
+┃ 
+┃ 🔞🔥 Vazou novinha cristã na Net 😋🔥🔞
+┃ 📂 • vídeo completo:
+┃ https://linkebr.com/oATK6RO4
+┃ 
+┃ 🔞🔥 +1 Lomotif 🤭🔥
+┃ 📂 • vídeo completo:
+┃ https://linkebr.com/jUsUTW4
+┃ 
+┃ 🔞🔥 Boluda caiu na Net 🙊🔥🔞
+┃ 📂 • vídeo completo:
+┃ https://linkebr.com/YP4cCyb
+┃ 
+┃ 🔞🔥 Caiu na Net 😐🔥🔞
+┃ 📂 • vídeo completo:
+┃ https://linkebr.com/V1QM
+┃ 
+┃ 🔞🔥 Naughty America Coleção
+┃ 📂 • Link pra baixar:
+┃ https://linkebr.com/anA6qjeL 
+┃ 
+┃ 🔞🔥 Atualização Polonesa do hype
+┃ 📂 • Link pra baixar:
+┃ https://linkebr.com/8tSWPCBQ
+┃ 
+┃ 🔞🔥 Morena rabuda 🙊🔥🔞
+┃ 📂 • vídeo completo:
+┃ https://linkebr.com/kjfPNSvw
+┃ 
+┃ 🔞🔥 Lomotif da peituda gostosa 😯🔞
+┃ 📂 • vídeo completo:
+┃ https://linkebr.com/u638
+┃ 
+┃ 🔞🔥 Ruivinha boqueteira 😋🔥
+┃ 📂 • vídeo completo:
+┃ https://linkebr.com/EParD4
+┃ 
+┃ 🔞🔥 Eu diria felicidade 😋🔥
+┃ 📂 • vídeo completo:
+┃ https://linkebr.com/WJYYPVlE
+┃ 
+┃ 🔞🔥 Belle Delphine mostrando a bucetinha
+┃  e o cuzinho ambos rosinhas 🤭🔥
+┃ 📂 • Link pra baixar:
+┃ https://linkebr.com/zczWyLB
+┃ 
+┃ 🔞🔥 Os 3 vídeos vazados
+┃  da Professora cibelly 😋
+┃ 📂 • Link pra baixar:
+┃ https://linkebr.com/lqBCO4Hb
+┃ 
+┃ 🔞🔥 🙊🔥🔞
+┃ 📂 • vídeo completo:
+┃ https://linkebr.com/0XD9BV
+┃ 
+┃ 🔞🔥 Lomotifzinho 🤭🔥🔞
+┃ 📂 • vídeo completo:
+┃ https://linkebr.com/tNxKs
+┃ 
+┃ 🔞🔥 a puta queria leite 🥴🔞
+┃ 📂 • vídeo completo:
+┃ https://linkebr.com/anGLYL
+┃  
+┃ 🔞🔥 eita porra 😳🔥🔞
+┃ 📂 • vídeo completo:
+┃ https://linkebr.com/OcjfXrSV
+┃ 
+┃ 🔞🔥 vaza vídeo dela tocando 
+┃ uma siririca 😛🔥🔞
+┃ 📂 • vídeo completo:
+┃ https://linkebr.com/YEHb74
+┃ 
+┃ 🔞🔥 5 de uma vez 😈🔞
+┃ 📂 • vídeo completo:
+┃ https://linkebr.com/NshP8
+┃ 
+┃ 🔞🔥 Vazou a siririca da estudante 🤭🤤🔥
+┃ 📂 • vídeo completo:
+┃ https://linkebr.com/SYgB
+┃ 
+┃ 🔞🔥 boqueteira caiu na Net 🙊🔥
+┃ 📂 • vídeo completo:
+┃ https://linkebr.com/5M5EY1
+┃ 
+┃ 🔞🔥 Pack da Carniello
+┃ 📂 • Link pra baixar:
+┃ https://linkebr.com/cXOJ1
+┃ 
+┃ 🔞🔥 Pack da Cami Brito
+┃ 📂 • Link pra baixar:
+┃ https://linkebr.com/b2SPmXl
+┃ 
+┃ 🔞🔥 Magrinha gostosa 🤤🔥
+┃ 📂 • vídeo completo:
+┃ https://linkebr.com/z38s
+┃ 
+┃ 🔞🔥 Novinha vazou 🤩🌸
+┃ 📂 • vídeo completo:
+┃ https://linkebr.com/yidCDvEs
+┃ 
+┃ 🔞🔥 Vazou o casalzinho 😁🔞
+┃ 📂 • vídeo completo:
+┃ https://linkebr.com/VbMWJ9
+┃ 
+┣━━━😎 𝒄𝒓𝒆𝒅𝒊𝒕𝒐𝒔 😎━━━╮
+┃ 𝒂𝒈𝒓𝒂𝒅𝒆𝒄𝒊𝒎𝒆𝒏𝒕𝒐 𝒑𝒂𝒓𝒂 
+┃ 𝒎𝒂𝒔𝒕𝒆𝒓𝒉𝒈 𝒑𝒐𝒓 𝒇𝒐𝒓𝒏𝒆𝒄𝒆𝒓
+┃ 𝒐𝒔 𝒑𝒂𝒄𝒌𝒔 
+┃ 𝒍𝒊𝒏𝒌 𝒕𝒆𝒍𝒆𝒈𝒓𝒂𝒎:
+┃ https://linkebr.com/mastrhg
+┃ 
+┃ 𝒍𝒊𝒏𝒌 𝒘𝒉𝒂𝒕𝒔:
+┃ https://linkebr.com/whatsmstr
+┃ 
+┃ 𝒍𝒊𝒏𝒌  𝒈𝒑 𝒕𝒆𝒍𝒆𝒈𝒓𝒂𝒎:
+┃ https://linkebr.com/gptelegram
+┃ 
+┃ 𝒍𝒊𝒏𝒌 𝒅𝒄:
+┃ https://linkebr.com/DcMaster
+┃ 
+┃ 𝒍𝒊𝒏𝒌 𝒊𝒏𝒔𝒕𝒂:
+┃ https://linkebr.com/InstaMaster
+┃ 
+┃ 𝒍𝒊𝒏𝒌 𝒕𝒘𝒊𝒕𝒕𝒆𝒓:
+┃ https://linkebr.com/TwitterMaster
+┃ 
+┃ 𝒍𝒊𝒏𝒌 𝒕𝒕𝒌:
+┃ https://linkebr.com/TTKMaster
+┃ 
+┃ vlw master ❤️
+┃ 
+┃ (𝒐𝒃𝒔: 𝒔𝒐 𝒐𝒔 𝒍𝒊𝒏𝒌𝒔 𝒑𝒂𝒓𝒂 𝒐𝒔
+┃ 𝒄𝒓𝒆𝒅𝒊𝒕𝒐𝒔 𝒇𝒐𝒊 𝒆𝒖 𝒒𝒖𝒆 𝒇𝒊𝒛,
+┃ 𝒐 𝒓𝒆𝒔𝒕𝒐 𝒅𝒐𝒔 𝒍𝒊𝒏𝒌𝒔 𝒇𝒐𝒊 𝒐 𝒎𝒂𝒔𝒕𝒆𝒓)
+┃ 
+╰━━━━━━━━━━━━━━━━━━━╯`
+    }
+    exports.packs = packs
